aa72d5627e7f354c70149197ef8bfd5efd10fd79
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-25 13:03:40 +0100

    MOD ports

--- scanner.py
+++ scanner.py
@@ -2,8 +2,8 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 
-colorSensor = ColorSensor(Port.S2)
-distanceSensor = UltrasonicSensor(Port.S1)
+colorSensor = ColorSensor(Port.S3)
+distanceSensor = UltrasonicSensor(Port.S2)
 
 
 def getDistance() -> int:
